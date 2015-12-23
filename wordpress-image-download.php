93940fa3a90dfa70432ed54c9f51cd1625bfd312
Chris Herbert <herbert@bermanco.com>
2015-12-23 12:31:10 -0500

    added actual code - forgot that little detail

---
+++ wordpress-image-download.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace bermanco\WordPressImageDownload;
+
+class WordPressImageDownload {
+
+	public $external_url;
+
+	public function __construct($url){
+
+		if (filter_var($url,FILTER_VALIDATE_URL)){
+			$this->external_url = $url;
+		}
+		else {
+			error_log('Invalid URL passed to WordPressImageDownload constructor');
+		}
+
+	}
+
+	public function create_media_attachment(){
+
+		if ($this->external_url){
+
+			$url = $this->external_url;
+			$tmp = download_url( $url );
+
+			$file_array = array(
+				'name' => basename( $url ),
+				'tmp_name' => $tmp
+			);
+
+			// Check for download errors
+			if ( is_wp_error( $tmp ) ) {
+				@unlink( $file_array[ 'tmp_name' ] );
+				return $tmp;
+			}
+
+			$id = media_handle_sideload( $file_array, 0 );
+
+			// Check for handle sideload errors.
+			if ( is_wp_error( $id ) ) {
+				@unlink( $file_array['tmp_name'] );
+				return $id;
+			}
+
+			return $id;
+
+		}
+
+	}
+
+	protected function get_file_extension($file){
+		return pathinfo($file['name'], PATHINFO_EXTENSION);
+	}
+
+}
+
